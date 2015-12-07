831a055ea97158abb47ba266782300531dddab60
PixelantTest <anton.perebyinis@pixelant.se>
2015-12-07 19:28:40 +0200

    Set up Galen and Nightwatch

--- Galen/scripts/change-grid.js
+++
@@ -1,1 +0,0 @@
-driver.findElement(By.cssSelector(".qbank_gridfilter_toggle.icon-grid6.icons")).click();
--- Nightwatch/scripts/globals.js
+++ Nightwatch/scripts/globals.js
@@ -1,11 +1,3 @@
 module.exports = {
-  // This is a iteration helper for 'filtersWork' test
-  iter: function (elems, browser, category, attr) {
-    elems.value.forEach(function (element, i) {
-      browser.elementIdAttribute(element.ELEMENT, attr, function(result){
-        this.assert.ok(result.value.indexOf(category) > -1,
-          "Item #" + (i+1) + " has " + attr + " set as " + category + "?");
-      })
-    })
-  },
+  
 }
