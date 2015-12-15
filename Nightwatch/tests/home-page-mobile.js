6b850652db42dd561e58010db9b0461fb01f04eb
codterpin <anton.perebyinis@gmail.com>
2015-12-16 01:36:27 +0200

    Add browserstack devices for Nightwatch and Galen

---
+++ Nightwatch/tests/home-page-mobile.js
@@ -0,0 +1,27 @@
+// Objects
+
+// Search
+var searchField = "#lst-ib";
+var searchButton = "button[name=btnG]";
+
+// Main area
+var mainArea = "#main";
+
+
+module.exports = {
+  'Open Google' : function (browser) {
+    browser
+      .url('http://www.google.com')
+      .waitForElementVisible('body', 1000)
+      .setValue(searchField, 'nightwatch')
+      .waitForElementVisible(searchButton, 1000);
+  },
+
+  'Search for Night Watch' : function (browser) {
+    browser
+      .click(searchButton)
+      .pause(1000)
+      .assert.containsText(mainArea, 'Night Watch', 'Found Night Watch result in main area?')
+      .end();
+  }
+};
