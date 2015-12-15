c06f98d9d94fc3a61e71dca45db54d12b3e1c58a
PixelantTest <anton.perebyinis@gmail.com>
2015-12-15 17:28:29 +0200

    Add saucelabs config for Galen and Nightwatch

--- Nightwatch/scripts/html-browserstack-reporter.js
+++ Nightwatch/scripts/html-browserstack-reporter.js
@@ -2,7 +2,7 @@ var path = require('path');
 var HtmlReporter = require('nightwatch-html-reporter');
 /* Same options as when using the built in nightwatch reporter option */
 var reporter = new HtmlReporter({
-  openBrowser: true,
+  openBrowser: false,
   reportsDirectory: path.resolve(__dirname, '../browserstack_reports')
 });
 
